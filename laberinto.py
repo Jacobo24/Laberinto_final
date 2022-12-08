de782e29b209dda5f07ba54136e772653ebb8439
Jacobo24 <jaccalgar@gmail.com>
2022-12-08 23:55:15 +0100

    ultimos camobios

--- laberinto.py
+++ laberinto.py
@@ -17,7 +17,7 @@ for i in lab:
     print(''.join(i))
 
 def recorre_laberinto(laberinto):
-    # Filas y columnas iniciales
+    # Fila y columnas iniciales
     fila = 0
     columna = 0
     # Dimensiones del laberinto
@@ -27,12 +27,12 @@ def recorre_laberinto(laberinto):
     while (fila < n-1 and columna < n-1):
         if movimientos[-1] != 'Arriba' and fila + 1 < n and laberinto[fila + 1][columna] != 'X':
             fila += 1
-            movimientos.append('Arriba')
-        elif movimientos[-1] != 'Izquierda' and columna + 1 < n and laberinto[fila][columna + 1] != 'X':
-            columna += 1
             movimientos.append('Abajo')
         elif movimientos[-1] != 'Abajo' and fila - 1 > 0 and laberinto[fila - 1][columna] != 'X':
             fila -= 1
+            movimientos.append('Arriba')
+        elif movimientos[-1] != 'Izquierda' and columna + 1 < n and laberinto[fila][columna + 1] != 'X':
+            columna += 1
             movimientos.append('Derecha')
         elif movimientos[-1] != 'Derecha' and columna - 1 > 0 and laberinto[fila][columna - 1] != 'X':
             columna -= 1
@@ -41,5 +41,5 @@ def recorre_laberinto(laberinto):
             movimientos.append('No hay salida')
     return movimientos
 
-# Mostrar Solución
+# Mostrar por pantalla la lista de movimientos
 print('Solución: ', recorre_laberinto(lab))
