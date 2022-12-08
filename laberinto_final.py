37d4c6a10a38b94249961a22343cf83b1a1daede
Jacobo24 <jaccalgar@gmail.com>
2022-12-08 23:06:32 +0100

    primero

---
+++ laberinto_final.py
@@ -0,0 +1,15 @@
+laberinto = [
+    [' ', 'X', 'X', 'X', 'X'], 
+    [' ', 'X', ' ', ' ', ' '],
+    [' ', 'X', ' ', 'X', ' '], 
+    [' ', ' ', ' ', 'X', ' '], 
+    ['X', 'X', 'X', 'X', 'S']
+    ]
+
+camino = ""
+laberinto = []
+laberinto.append([])
+res = []
+maxh = 0
+maxv = 0
+piso = []
